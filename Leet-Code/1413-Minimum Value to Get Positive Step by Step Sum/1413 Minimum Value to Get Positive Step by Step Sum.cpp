5b197bfb5248b1326136756d17952000b7499a3f
Murugan K <83358275+murugan-k-0204@users.noreply.github.com>
2021-06-15 15:38:38 +0530

    Create 1413 Minimum Value to Get Positive Step by Step Sum.cpp

---
+++ Leet-Code/1413-Minimum Value to Get Positive Step by Step Sum/1413 Minimum Value to Get Positive Step by Step Sum.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minStartValue(vector<int>& nums) {
+        int minimum,a;
+        minimum=a = nums[0];
+        for (int i = 1; i < nums.size(); i++)
+        {
+            a+=nums[i];
+            minimum = min(minimum,a);    
+        }
+        if(minimum>0) return 1;
+        return abs(minimum)+1;
+    }
+};

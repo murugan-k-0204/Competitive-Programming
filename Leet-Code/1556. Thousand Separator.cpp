de9d455155293e2ebbffa1f3222659925be859b9
Murugan K <83358275+murugan-k-0204@users.noreply.github.com>
2021-06-15 15:04:06 +0530

    Create 1556. Thousand Separator.cpp

---
+++ Leet-Code/1556. Thousand Separator.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string thousandSeparator(int n) {
+        string s = to_string(n),res;
+        for (int i = 0; i < s.size(); i++)
+        {
+            if(i>0 && (s.size()-i)%3==0)
+                res+='.';
+            res+=s[i];
+        }
+        return res;
+    }
+};

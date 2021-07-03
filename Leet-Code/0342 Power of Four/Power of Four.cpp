a9f16d333c0af9893a5ce43dd852c0648adb43fc
Murugan K <83358275+murugan-k-0204@users.noreply.github.com>
2021-07-03 21:52:54 +0530

    Update Power of Four.cpp

--- Leet-Code/0342 Power of Four/Power of Four.cpp
+++ Leet-Code/0342 Power of Four/Power of Four.cpp
@@ -4,5 +4,11 @@ public:
         
 		int mask = 0b01010101010101010101010101010101;
 		return num>0 && (num&(num - 1)) == 0 && (num | mask) == mask;
+	    
+	    // num>0  - >  check negative number
+	    // num& num(num-1)==0 check if is a power of 2
+	    		//16 -> 10000 & 1111 == 0 for this we used.
+	    // num| mask  - > check whether it is in that sequence.
+	    
     }
 };

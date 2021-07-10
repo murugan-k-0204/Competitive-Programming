e2dfcb4d03a5c8b5ec8edc07c7f1937207742d0a
Murugan K <83358275+murugan-k-0204@users.noreply.github.com>
2021-07-10 19:31:04 +0530

    Create 1 Two Sum.cpp

---
+++ Leet-Code/1 Two Sum/1 Two Sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> hashtable;
+        for(int i=0;i<nums.size();i++){
+            hashtable[nums[i]] = i;
+        }
+        for(int i=0;i<nums.size();i++){
+            int second = target - nums[i];
+            if(hashtable[second]){
+                if(hashtable[second]==i) continue;
+                return {i,hashtable[second]};
+            }
+        }
+    return {0,0};
+    }
+};

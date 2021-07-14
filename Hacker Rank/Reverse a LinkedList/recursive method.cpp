9c87dd3b66b6ce0515d17ee399f766248441d03b
Murugan K <83358275+murugan-k-0204@users.noreply.github.com>
2021-07-14 15:48:24 +0530

    Update recursive method.cpp

--- Hacker Rank/Reverse a LinkedList/recursive method.cpp
+++ Hacker Rank/Reverse a LinkedList/recursive method.cpp
@@ -1,7 +1,7 @@
 SinglyLinkedListNode* reverse(SinglyLinkedListNode* llist) {
     if(llist->next==nullptr||llist==nullptr) 
         return llist;
-    SinglyLinkedListNode* new_head = reverse(llist->next);
+    auto new_head = reverse(llist->next);
     llist->next->next = llist;
     llist->next = nullptr;
     return new_head;    

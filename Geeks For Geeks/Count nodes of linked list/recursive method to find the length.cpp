983109ba15f7d8b3c621f0703f07357b9b51a48d
Murugan K <83358275+murugan-k-0204@users.noreply.github.com>
2021-07-14 09:17:41 +0530

    Delete recursive method to find the length.cpp

--- Geeks For Geeks/Count nodes of linked list/recursive method to find the length.cpp
+++
@@ -1,9 +0,0 @@
-class Solution
-{
-    public:
-    int getCount(struct Node* head,int len=1){
-        if(head->next==NULL)  
-            return len;
-        return getCount(head->next,len+1);
-    }
-};

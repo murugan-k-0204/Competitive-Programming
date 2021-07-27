2d36440ff78d788682582fcc45a4251824130bab
Murugan K <83358275+murugan-k-0204@users.noreply.github.com>
2021-07-27 21:21:33 +0530

    Create recursive method.cpp

---
+++ Hacker Earth/Data Structures/Arrays/Print Array in Reverse/recursive method.cpp
@@ -0,0 +1,15 @@
+  
+#include<iostream>
+using namespace std;
+
+void solve(int n){
+    if(n==0) return ;
+    int a; cin>>a;
+    solve(n-1);
+    cout<<a<<endl;
+}
+
+int main(){
+    int n; cin>>n;
+    solve(n);
+}
